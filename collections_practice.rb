2332e940fa711b3c9389f4e97d8121b61ca2ee5f
nathan hains <nathanhains14@yahoo.com>
2021-02-04 02:01:51 -0500

    commit tree b424345e005ed45cf355c5e4958e5f83ddf8d7ed

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,61 @@
+def sort_array_asc(array)
+    array.sort
+end
+def sort_array_desc(array)
+    array.sort do |a,b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a,b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements(array)
+    temp = array[1];
+	array[1] = array[2];
+	array[2] = temp;
+    return array;
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+     new_array = []
+     array.each do |num|
+        num.split
+        num[2] = "$"
+        new_array << num   
+    end
+    new_array
+end
+
+def find_a(array)
+    array.select do |num|
+        num[0] == "a"
+    end
+end
+
+def sum_array(array)
+    sum = 0
+    array.each { |a| sum+=a }
+    sum
+end
+
+def add_s(array)
+    new_array = []
+    array.each do |num|
+        new_array << "#{num}s"
+    end
+    new_array[1] = "feet"
+    new_array
+end
+
+
+
+
 
